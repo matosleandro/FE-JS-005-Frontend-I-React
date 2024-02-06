5020ad3c440c2e5db236e6c91efe0c59948af07a
Leandro Matos <leandro.matos@live.com>
2024-02-06 00:05:41 -0300

    add: navbar

--- aula_02/src/App.jsx
+++ aula_02/src/App.jsx
@@ -1,10 +1,14 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <h2>Movie DB</h2>
+      <Outlet />
     </div>
   )
 }
