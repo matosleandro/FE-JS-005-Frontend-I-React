048027837f105bcadb07ff527bfe492a9322d21b
Leandro Matos <leandro.matos@live.com>
2024-02-06 00:53:12 -0300

    add: card dos filmes, ainda sem o css

--- aula_02/src/App.jsx
+++ aula_02/src/App.jsx
@@ -1,16 +1,15 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <Navbar />
-      <h2>Movie DB</h2>
       <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- aula_02/src/pages/Home.jsx
+++ aula_02/src/pages/Home.jsx
@@ -1,5 +1,39 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
-  return <div>Home</div>;
+  // os melhores filmes e os com melhor nota na API  
+  const [topMovies, setTopMovies] = useState([])
+  // função assincrona para fazer a requisição onde vai esperar um url da api
+  const getTopRatedMovies = async (url) => {
+  // aqui será uma resposta baseado no await fetch
+    const res = await fetch(url);
+  // aqui vou receber dados mas preciso transformar esses dados em await de objetos em js
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, [])
+
+  return (
+    <div clasName="container">
+        <h2 clasName="title">Melhores Filmes:</h2>
+        <div className="movies-container">
+            {topMovies.length === 0 && <p>Carregando...</p>}
+            {topMovies.length > 0 && 
+                topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        </div>
+    </div>
+  );
 };
 
 export default Home
